89d7d7f985941c788176a5c6cc669ff76f550631
Eric <eric.m.lombardo@gmail.com>
2020-11-16 08:45:58 -0500

    commit tree 42a471482aa3d42f532ffedbd37fc0a80faeac50

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  length = array.length
+  case length
+    when 1
+      array.join
+    when 2
+        array.join(" and ")
+    else
+        last_item = array.pop
+        array.join(', ') + ', and ' + last_item
+    end
+  end
 
-end
+
+
+
